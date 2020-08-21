c05498dfdd2d8d56e7b6277d333a624b5418209a
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-08-21 13:36:13 +0530

    Added ‘Home’ component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+import Home from "./screens/home/Home";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div></div>
+    <Home></Home>
   </React.StrictMode>,
   document.getElementById("root")
 );
