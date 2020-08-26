5760307e5433b23ef6b708bf9b39ecaad7221d78
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-08-26 18:05:51 +0530

    Added 'Movie Name' filter on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -6,6 +6,12 @@ import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
 import moviesData from "./../../common/movieData";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import FormControl from "@material-ui/core/FormControl";
+import Typography from "@material-ui/core/Typography";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
 
 const styles = (theme) => ({
   root: {
@@ -29,9 +35,26 @@ const styles = (theme) => ({
     cursor: "pointer",
     transform: "translateZ(0)",
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 240,
+    maxWidth: 240,
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
 });
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = { movieName: "" };
+  }
+
+  movieNameChangeHandler = (event) => {
+    this.setState({ movieName: event.target.value });
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -40,6 +63,7 @@ class Home extends Component {
         <div className={classes.upcomingMoviesHeading}>
           <span>Upcoming Movies</span>
         </div>
+
         <GridList cols={5} className={classes.gridListUpcomingMovies}>
           {moviesData.map((movie) => (
             <GridListTile key={movie.id}>
@@ -83,7 +107,24 @@ class Home extends Component {
               ))}
             </GridList>
           </div>
-          <div className="right"></div>
+          <div className="right">
+            <Card>
+              <CardContent>
+                <FormControl className={classes.formControl}>
+                  <Typography className={classes.title} color="textSecondary">
+                    FIND MOVIES BY:
+                  </Typography>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+                  <Input
+                    id="movieName"
+                    onChange={this.movieNameChangeHandler}
+                  ></Input>
+                </FormControl>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </div>
     );
