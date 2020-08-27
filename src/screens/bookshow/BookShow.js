000af25a9388b055a59a82f05ff0b73f3090a588
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-08-27 22:49:07 +0530

    Added Location on book show page

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -4,10 +4,32 @@ import Header from "../../common/header/Header";
 import Typography from "@material-ui/core/Typography";
 import "./BookShow.css";
 import Home from "../home/Home";
+import language from "../../common/language";
+import location from "../../common/location";
+import showDate from "../../common/showDate";
+import showTime from "../../common/showTime";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 class BookShow extends Component {
+  constructor() {
+    super();
+    this.state = {
+      location: "",
+    };
+  }
+
   backToDetailsHandler = () => {
     ReactDOM.render(<Home />, document.getElementById("root"));
+  };
+
+  locationChangeHandler = (event) => {
+    this.setState({ location: event.target.value });
   };
 
   render() {
@@ -18,6 +40,27 @@ class BookShow extends Component {
           <Typography className="back" onClick={this.backToDetailsHandler}>
             &#60; Back to Movie Details
           </Typography>
+          <Card className="cardStyle">
+            <CardContent>
+              <Typography variant="headline" component="h2">
+                BOOK SHOW
+              </Typography>
+              <br />
+              <FormControl required className="formControl">
+                <InputLabel htmlFor="location">Choose Location:</InputLabel>
+                <Select
+                  value={this.state.location}
+                  onChange={this.locationChangeHandler}
+                >
+                  {location.map((loc) => (
+                    <MenuItem key={"loc" + loc.id} value={loc.location}>
+                      {loc.location}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </CardContent>
+          </Card>
         </div>
       </div>
     );
