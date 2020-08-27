44621b881de30daa9f6df093b0debc57be15d444
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-08-27 23:18:06 +0530

    Added Unit Price, Total Price and Book Show button on book show page

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -15,6 +15,7 @@ import InputLabel from "@material-ui/core/InputLabel";
 import Input from "@material-ui/core/Input";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/Button";
 
 class BookShow extends Component {
   constructor() {
@@ -24,6 +25,9 @@ class BookShow extends Component {
       language: "",
       showDate: "",
       showTime: "",
+      tickets: 0,
+      unitPrice: 500,
+      availableTickets: 20,
     };
   }
 
@@ -45,6 +49,10 @@ class BookShow extends Component {
 
   showTimeChangeHandler = (event) => {
     this.setState({ showTime: event.target.value });
+  };
+
+  ticketsChangeHandler = (event) => {
+    this.setState({ tickets: event.target.value });
   };
 
   render() {
@@ -121,6 +129,32 @@ class BookShow extends Component {
               </FormControl>
               <br />
               <br />
+              <FormControl required className="formControl">
+                <InputLabel htmlFor="tickets">
+                  Tickets: ( {this.state.availableTickets} available )
+                </InputLabel>
+                <Input
+                  id="tickets"
+                  value={this.state.tickets !== 0 ? this.state.tickets : ""}
+                  onChange={this.ticketsChangeHandler}
+                />
+              </FormControl>
+              <br />
+              <br />
+              <Typography>Unit Price: Rs. {this.state.unitPrice}</Typography>
+              <br />
+              <Typography>
+                Total Price: Rs. {this.state.unitPrice * this.state.tickets}
+              </Typography>
+              <br />
+              <br />
+              <Button
+                variant="contained"
+                onClick={this.bookShowButtonHandler}
+                color="primary"
+              >
+                BOOK SHOW
+              </Button>
             </CardContent>
           </Card>
         </div>
