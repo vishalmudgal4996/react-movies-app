e3c539c0c356fac86e5beae2ca6adc954cfcf928
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-08-30 20:57:53 +0530

    “Added movie name & genres in the event handler for 'Apply Filters' button”

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -83,6 +83,16 @@ class Home extends Component {
     this.props.history.push("/movie/" + movieId);
   };
 
+  filterApplyHandler = () => {
+    let queryString = "?status=RELEASED";
+    if (this.state.movieName !== "") {
+      queryString += "&title=" + this.state.movieName;
+    }
+    if (this.state.genres.length > 0) {
+      queryString += "&genres=" + this.state.genres.toString();
+    }
+  };
+
   UNSAFE_componentWillMount() {
     // GET Upcoming Movies
 
@@ -289,7 +299,11 @@ class Home extends Component {
                 <br />
                 <br />
                 <FormControl className={classes.formControl}>
-                  <Button variant="contained" color="primary">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => this.filterApplyHandler()}
+                  >
                     APPLY
                   </Button>
                 </FormControl>
